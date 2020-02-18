69a09948f757c82b383176a37644f640da20721c
jetpackpony <jetpackpony@gmail.com>
2020-02-18 21:30:44 +0300

    Add video rendering and mocking API

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 import reddit from './reddit-api';
 
+const initialState = {
+  videos: [],
+  currentId: 0
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SHOW_NEXT':
+      return {
+        ...state,
+        currentId: state.currentId + 1
+      };
+    case 'SET_NEW_VIDEOS':
+      return {
+        videos: action.videos,
+        currentId: 0
+      };
+    default:
+      throw new Error();
+  }
+};
+
 function App() {
-  reddit
-    .init()
-    .getVideos({
-      subreddit: "WatchPeopleDieInside"
-    })
-    // .map((p) => p.title)
-    .then((list) => list[1])
-    .then((post) => {
-      console.log(post);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const needsToLoadVideos = state.videos.length === 0 || state.videos.length === (state.currentId + 1);
+  
+  console.log("Rendering: ", state);
+  if (needsToLoadVideos) {
+    console.log("Loading videos. Mocking API: ", process.env.REACT_APP_MOCK_API);
+    (
+      // (process.env.REACT_APP_MOCK_API)
+      (true)
+        ? import('./api_output.json').then((list) => list.default)
+        : reddit
+            .init()
+            .getVideos({
+              subreddit: "WatchPeopleDieInside"
+            })
+    ).then((list) => {
+      dispatch({ type: "SET_NEW_VIDEOS", videos: list });
     })
     .catch((e) => console.log("Error: ", e));
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to dd.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        (state.videos[state.currentId])
+          ? <video autoPlay controls src={state.videos[state.currentId].media.reddit_video.fallback_url} width={1024} height={576}></video>
+          : "Loading..."
+      }
+      <button onClick={() => dispatch({ type: "SHOW_NEXT" })}>
+        {state.currentId + 1} / {state.videos.length} Next >>
+      </button>
+    </>
   );
 }
 
