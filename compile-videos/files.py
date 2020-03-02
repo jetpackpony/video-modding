72c670ebfd52518954acb3d63690156c6bb18931
jetpackpony <jetpackpony@gmail.com>
2020-03-03 00:07:43 +0300

    Iterate over all collections ready to montage

--- compile-videos/files.py
+++ compile-videos/files.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+import os
+import re
 
 def create_file_and_dir(dir, file):
   create_dir(dir)
@@ -10,6 +11,17 @@ def create_dir(dir):
   if not dir.is_dir():
     dir.mkdir()
 
+jsonReg = re.compile(r".+\.json$")
+def list_json_files(dir):
+  files = []
+  with os.scandir(dir) as entries:
+    for entry in entries:
+      if jsonReg.search(entry.name):
+        files.append(entry.name)
+  return files
+
+def move_file(fr, to):
+  os.rename(fr, to)
 
 if __name__ == "__main__":
   create_file_and_dir("../../dev", "test.json")
--- compile-videos/montage.py
+++ compile-videos/montage.py
@@ -2,6 +2,8 @@ from pprint import pprint
 from moviepy.editor import *
 from json_ops import read_json
 from editing import *
+from env import is_prod
+from files import create_dir, list_json_files, move_file
 
 # resolution = (320, 240)
 resolution = (1920, 1080)
@@ -9,10 +11,19 @@ resolution = (1920, 1080)
 trans = VideoFileClip("transition_static_0.3sec.mp4")
 trans = pad_clip_to_size(resolution, trans)
 
-filename = "../../collections/25_Feb_2020_17_34_23.json"
-collection = read_json(filename)
+collections_path = "../../collections/" if is_prod() else "../../dev_collections/"
+outputs_path = "../../outputs/" if is_prod() else "../../dev_outputs/"
+create_dir(outputs_path)
 
-clips = list(map(lambda info: process_clip(info, resolution), collection['videos']))
-out = concatenate_videoclips(clips, method="compose", transition=trans)
+cols = list_json_files(collections_path)
 
-out.write_videofile(collection['id'] + ".mp4", threads = 4, fps = 30)
+for filename in cols:
+  collection = read_json(collections_path + filename)
+  dir_path = outputs_path + collection['id'] + "/"
+  create_dir(dir_path)
+
+  clips = list(map(lambda info: process_clip(info, resolution), collection['videos']))
+  out = concatenate_videoclips(clips, method="compose", transition=trans)
+  out.write_videofile(dir_path + collection['id'] + ".mp4", threads = 4, fps = 30)
+
+  move_file(collections_path + filename, dir_path + filename)
