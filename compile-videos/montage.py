5664a115c294bd2728d298561f86b331a7578ca3
jetpackpony <jetpackpony@gmail.com>
2020-03-03 17:51:04 +0300

    Add a try-catch block to montage

--- compile-videos/montage.py
+++ compile-videos/montage.py
@@ -19,17 +19,21 @@ create_dir(outputs_path)
 cols = list_json_files(collections_path)
 
 for filename in cols:
-  collection = read_json(collections_path + filename)
-  dir_path = outputs_path + collection['id'] + "/"
-  create_dir(dir_path)
+  try:
+    collection = read_json(collections_path + filename)
+    dir_path = outputs_path + collection['id'] + "/"
+    create_dir(dir_path)
 
-  def process_video(info):
-    vid_path = videos_path + info['id'] + ".mp4"
-    make_screenshots(dir_path, vid_path, info['id'], info["screenshots"])
-    return process_clip(info, resolution)
+    def process_video(info):
+      vid_path = videos_path + info['id'] + ".mp4"
+      make_screenshots(dir_path, vid_path, info['id'], info["screenshots"])
+      return process_clip(info, resolution)
 
-  clips = list(map(process_video, collection['videos']))
-  out = concatenate_videoclips(clips, method="compose", transition=trans)
-  out.write_videofile(dir_path + collection['id'] + ".mp4", threads = 4, fps = 30)
+    clips = list(map(process_video, collection['videos']))
+    out = concatenate_videoclips(clips, method="compose", transition=trans)
+    out.write_videofile(dir_path + collection['id'] + ".mp4", threads = 4, fps = 30)
 
-  move_file(collections_path + filename, dir_path + filename)
+    move_file(collections_path + filename, dir_path + filename)
+  except OSError as err:
+    print("!!!!!!!! Couldn't process %s" % filename, err)
+
