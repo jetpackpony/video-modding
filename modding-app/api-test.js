e22cbb2868aa21c219c2ace77abc42f4d646ca29
jetpackpony <jetpackpony@gmail.com>
2020-03-04 20:08:56 +0300

    Update api test

--- modding-app/api-test.js
+++ modding-app/api-test.js
@@ -1,13 +1,22 @@
-const reddit = require('./src/reddit-api').init();
+const reddit = require('./src/reddit-api');
 
 reddit
+  .init()
   .getVideos({
     subreddit: "WatchPeopleDieInside",
+    listingType: reddit.LISTING_TYPES.HOT,
     // after: "t3_f6waz7"
     // before: "t3_f6grvp"
     // before: "t3_f6w255"
 
   })
-  .map((p) => [ p.name, p.title ])
-  .then(console.log);
+  .then((res) => {
+    if (!res.done) {
+      console.log(`Found ${res.value.length} documents`);
+    } else {
+      console.log(`Reached end of listing`);
+    }
+  })
+  // .map((p) => [ p.name, p.title ])
+  // .then(console.log);
  
