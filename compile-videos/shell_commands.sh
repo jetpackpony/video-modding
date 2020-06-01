2b9210dbc1119750feb5478677f5e3c62d474b0c
jetpackpony <jetpackpony@gmail.com>
2020-06-01 14:24:14 +0300

    Add a list of shell commands

---
+++ compile-videos/shell_commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# First, start pipenv shell
+# pipenv shell
+
+# Then run the script that collects and downloads videos
+# PROD=true py collect_videos.py
+
+# Then run the montage script that outputs the final
+# PROD=true py montage.py
